6386f639e3b9b6de872c82cb4b740cb63adbdd7f
prx <1243794700@qq.com>
2020-07-26 11:43:12 +0800

    add 5and6

--- githelp.bat
+++ githelp.bat
@@ -2,12 +2,14 @@
 rem 后续命令使用的是：UTF-8编码 支持中文输出
 chcp 65001
 :loop
-set /p option=0,develop mode; 1,提交 2,提交并推送 3,git初始化 4,添加远程仓库
+set /p option=0,develop mode; 1,提交 2,提交并推送 3,git初始化 4,添加远程仓库 5,查看版本 6，版本恢复   :
 if "%option%"=="0" goto develop
 if "%option%"=="1" goto add&commit
 if "%option%"=="2" goto push
 if "%option%"=="3" goto init
 if "%option%"=="4" goto add_remote
+if "%option%"=="5" goto rev-parse
+if "%option%"=="6" goto recover
 
 :develop
 start cmd
@@ -48,5 +50,22 @@ echo %remote_rep%
 set /p ssh=远程仓库ssh:
 echo %ssh%
 git remote add %remote_rep% %ssh%
+
+goto loop
+
+:rev-parse
+echo 当前分支：
+git branch
+echo 当前版本号：
+git rev-parse HEAD
+echo ----历史版本----
+git log
+echo ----提交记录----
+git reflog
+goto loop
+
+:recover
+set /p rev-parse=输入版本号：
+git reset --hard %rev-parse%
 
 goto loop
